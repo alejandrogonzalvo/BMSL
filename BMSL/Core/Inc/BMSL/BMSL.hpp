4547883318e8045676adb4ffeb83fea46f03e327
alejandrogonzalvo <alejandrogonhid@protonmail.com>
2023-05-17 11:29:36 +0200

    update BMSL

--- BMSL/Core/Inc/BMSL/BMSL.hpp
+++ BMSL/Core/Inc/BMSL/BMSL.hpp
@@ -7,6 +7,7 @@ private:
     BMSA bms;
     HalfBridge dclv;
     BMSLStateMachine state_machine;
+    uint8_t avionics_current_sensor;
 
 public:
     BMSLLeds leds;
@@ -14,13 +15,21 @@ public:
     BMSL(
         SPI::Peripheral& bms_spi_pin,
         array<reference_wrapper<Pin>, 5> dclv_pins,
-        array<reference_wrapper<Pin>, 7> led_pins
+        array<reference_wrapper<Pin>, 7> led_pins,
+        Pin& avionics_current_sensor
         );
+
+    float get_avionics_current();
 };
 
-BMSL::BMSL(SPI::Peripheral& bms_spi_pin, array<reference_wrapper<Pin>, 5> dclv_pins, array<reference_wrapper<Pin>, 7> led_pins) {
+BMSL::BMSL(SPI::Peripheral& bms_spi_pin, array<reference_wrapper<Pin>, 5> dclv_pins, array<reference_wrapper<Pin>, 7> led_pins, Pin& avionics_current_sensor) {
     bms = BMSA(bms_spi_pin);
     dclv = HalfBridge(dclv_pins[0], dclv_pins[1], dclv_pins[2], dclv_pins[3], dclv_pins[4]);
     leds = BMSLLeds(led_pins[0], led_pins[1], led_pins[2], led_pins[3], led_pins[4], led_pins[5], led_pins[6]);
     state_machine = BMSLStateMachine();
+    this->avionics_current_sensor = ADC::inscribe(avionics_current_sensor).value();
+}
+
+float BMSL::get_avionics_current() {
+    return ADC::get_value(avionics_current_sensor);
 }
--- BMSL/Core/Src/main.cpp
+++ BMSL/Core/Src/main.cpp
@@ -7,19 +7,25 @@
 
 
 int main(void) {
+	float avionics_current = 0;
+	HeapPacket avionics_current_packet = HeapPacket(300, &avionics_current);
+	
 	array<reference_wrapper<Pin>, 5> dclv_pins = {DO_INV_PWM_H1, DO_INV_PWM_L1, DO_INV_PWM_H2, DO_INV_PWM_L2, GATE_DRIVERS_EN};
 	array<reference_wrapper<Pin>, 7> led_pins = {LED_LOW_CHARGE, LED_FULL_CHARGE, LED_SLEEP, LED_FLASH, LED_CAN, LED_FAULT, LED_OPERATIONAL};
 	BMSL bmsl(
 		SPI::spi3,
 		dclv_pins,
-		led_pins
+		led_pins,
+		AVIONICSCURRENTSENSORFW
 	);
 
 	STLIB::start();
 
 
+
 	while(1) {
 		bmsl.leds.can.toggle();
+		
 		HAL_Delay(1000);
 		STLIB::update();
 	}
